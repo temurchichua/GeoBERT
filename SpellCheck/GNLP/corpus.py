2cc6a0e675341767e80c08481004df70c0be1bdf
temurchichua <temur.chichua@iliauni.edu.ge>
2020-10-19 19:56:34 +0400

    restructured

--- SpellCheck/GNLP/corpus.py
+++ SpellCheck/GNLP/corpus.py
@@ -18,7 +18,9 @@ log.info('Welcome to the Georgian NLP toolset demo')
 
 printable = set(string.printable)
 
-data_folder = Path("data/")
+def file_path(file_name):
+    """generates path relative to the GNLP Package and modules"""
+    return os.path.join(os.path.dirname(__file__), file_name)
 
 def not_printable(word):
     """
@@ -50,7 +52,7 @@ def sizeof_fmt(file_size, suffix='B'):
 class Corpus():
     def __init__(self, stop_words='stops.txt'):
 
-        self.stop_words = set(line.strip() for line in open("data/stops.txt", encoding='utf-8'))
+        self.stop_words = set(line.strip() for line in open(file_path("data/stops.txt"), encoding='utf-8'))
         self.sequence = []
         self.prepro_sequence = []
         self.tokens = []
@@ -71,8 +73,7 @@ class Corpus():
         """collects all improtant data about file from file name and stores in attribute
         !!!needs to be separated in it's own class!!!"""
         cls.file_name = file_name
-        cls.basedir = os.path.abspath(os.path.dirname(file_name))
-        cls.path = os.path.join(cls.basedir, cls.file_name)
+        cls.path = file_path(file_name)
         cls.status = os.stat(cls.path)
         cls.file_size = sizeof_fmt(cls.status.st_size)
         return cls()
@@ -141,14 +142,14 @@ class Corpus():
     def save_corpus(self, name='corpus'):
         data = copy.deepcopy(self)
         del data.stop_words
-        with open(f'/data/pickles/{name}.pickle', 'wb') as destination_file:
+        with open(file_path(f'/data/pickles/{name}.pickle'), 'wb') as destination_file:
             # Step 3
             pickle.dump(data, destination_file, protocol=pickle.HIGHEST_PROTOCOL)
 
     @staticmethod
     def load_corpus(name='corpus'):
         # Step 2
-        with open(f'/data/pickles/{name}.pickle', 'rb') as file_location:
+        with open(file_path(f'/data/pickles/{name}.pickle'), 'rb') as file_location:
             corp = pickle.load(file_location)
             return corp
 
